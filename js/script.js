4bb0877a6fe37c23bc80bd662b79cd4aec30d2b5
Leo9723 <pala9761@gmail.com>
2022-12-07 15:24:29 +0100

    termine esercizio con bonus e superbonus partendo dalla base fatta il giorno precedente

--- js/script.js
+++ js/script.js
@@ -12,10 +12,26 @@ function cellTab(numero) {
     return cell;
 }
 
+function bombsarray(min, max) {
+    let bombs = []
+    let i = 0
+    while(i<16){
+        let number = Math.floor(Math.random() * (max - min + 1)) + min
+
+        if (!bombs.includes(number)){
+            bombs.push(number)
+            i++
+        }
+    }
+
+    return bombs;
+}
 
 button.addEventListener('click', function(){
 
     document.querySelector('.container').innerHTML = ''
+
+    container.classList.remove('pointer')
     
     let number = parseInt(document.getElementById('number').value);
 
@@ -23,49 +39,86 @@ button.addEventListener('click', function(){
   
     
     if(number == 100) {
-        for(let i = 0; i<number; i++){
         
+        let arraybombs = []
+
+        arraybombs = bombsarray(1, 84)
+        let score = []
+        console.log(arraybombs)
+        for(let i = 0; i<number; i++){
+
+            
             const cella = cellTab(i+1)
             cella.classList.add('c-100')
             container.appendChild(cella);
             
             cella.addEventListener('click', function(){
-                cella.classList.add('red-box')
-                console.log(this.innerText)
+                if(arraybombs.includes(parseInt(this.innerText))){
+                    cella.classList.add('red-box')
+                    container.classList.add('pointer')
+                    document.getElementById('final-score').innerHTML = 'il tuo punteggio è ' + score.length
+                }
+                else{
+                    cella.classList.add('blue-box')
+                    score.push(this.innerText)
+                    console.log(score)
+                }    
+                if(score.length == 33) {
+                    document.getElementById('final-score').innerHTML = 'Hai vinto!'
+                }           
             }) 
-            
-            
-        
         }
     }
     else if(number == 81) {
+        arraybombs = bombsarray(1, 65)
+        let score = []
+        console.log(arraybombs)
         for(let i = 0; i<number; i++){
             const cella = cellTab(i+1)
             cella.classList.add('c-81')
             container.appendChild(cella);
 
             cella.addEventListener('click', function(){
-                cella.classList.add('red-box')
-                console.log(this.innerText)
+                if(arraybombs.includes(parseInt(this.innerText))){
+                    cella.classList.add('red-box')
+                    container.classList.add('pointer')
+                    document.getElementById('final-score').innerHTML = 'il tuo punteggio è ' + score.length
+                }
+                else{
+                    cella.classList.add('blue-box')
+                    score.push(this.innerText)
+                    console.log(score)
+                } 
+                if(score.length == 33) {
+                    document.getElementById('final-score').innerHTML = 'Hai vinto!'
+                } 
             }) 
-
-
-
         }
     }
     else {
+        arraybombs = bombsarray(1, 49)
+        let score = []
+        console.log(arraybombs)
         for(let i = 0; i<number; i++){
             const cella = cellTab(i+1)
             cella.classList.add('c-49')
             container.appendChild(cella);
 
             cella.addEventListener('click', function(){
-                cella.classList.add('red-box')
-                console.log(this.innerText)
-            }) 
-
-
-            
+                if(arraybombs.includes(parseInt(this.innerText))){
+                    cella.classList.add('red-box')
+                    container.classList.add('pointer')
+                    document.getElementById('final-score').innerHTML = 'il tuo punteggio è ' + score.length
+                }
+                else{
+                    cella.classList.add('blue-box')
+                    score.push(this.innerText)
+                    console.log(score)
+                }
+                if(score.length == 33) {
+                    document.getElementById('final-score').innerHTML = 'Hai vinto!'
+                } 
+            })   
         }
     }
     
@@ -77,3 +130,4 @@ button.addEventListener('click', function(){
 /* element.addEventListener('click', function(){
     element.classList.add('red-box')
 }) */
+
